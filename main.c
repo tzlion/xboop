d5d6889af224d97b822f1c2e568881ee0d9da72d
taizou <cactusgunman@gmail.com>
2024-09-05 21:43:18 +0100

    more sleep zzz

--- main.c
+++ main.c
@@ -25,7 +25,8 @@ void     CmdFtell(uint32_t handle);
 
 //---------------------------------------------------------------------------
 int sleep1 = 10;
-int bigsleep = 100;
+int bigsleep = 1000; // 1s sleep done after completion of every print and fwrite command
+// Mitigates the problem of the subsequent command being missed sometimes
 
 FILE* fpSave[255];
 int nextHandle = 0;
@@ -180,6 +181,8 @@ int main(int argc, char* argv[])
 
 	printf("MultiBoot done\n\n");
 
+    millisleep(bigsleep);
+
 	// select cmd
 	for(;;)
 	{
@@ -231,6 +234,7 @@ int main(int argc, char* argv[])
 			break;
 
 		default:
+            // printf("%08x", r);
 			break;
 		}
 
